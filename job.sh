0ccc6563b49e447aecd7d99f0194d602199a8206
covaliandrei <covali.andrei.box@gmail.com>
2023-12-20 09:44:01 +0200

    job.sh script will run on infinite loop and check if it's 09:01 then fire up the main.py

--- job.sh
+++ job.sh
@@ -2,8 +2,11 @@
 
 while :
 do
-    echo "qwerty"
+    echo "Wait for 09:01 next morning"
+    while [ $(date +%H:%M) != "09:01" ]; do sleep 1; done
+
+    echo "Starting main.py"
     cd /home/andrei/PycharmProjects/Update-NS-on-NIC.MD/
     python3 main.py
-    while [ $(date +%H:%M) != "09:01" ]; do sleep 1; done
 done
+
